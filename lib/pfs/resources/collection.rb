d0d5a7101e604b77146746352632e13c00fd53c8
Raúl Villares <raul@devengo.com>
2023-12-20 10:37:11 +0100

    Ensure the collection creation for nil attribute

--- lib/pfs/resources/collection.rb
+++ lib/pfs/resources/collection.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module PFS
   module Resources
     class Collection
@@ -8,8 +6,9 @@ module PFS
 
       def initialize(response, item_klass, attributes_collection = [])
         @response = response
-        @attributes_collection = attributes_collection
-        @items = attributes_collection.map { |attributes_item| item_klass.new(nil, attributes_item) }
+        @items = Array(attributes_collection).map { |attributes_item|
+          item_klass.new(nil, attributes_item)
+        }
       end
 
       def each(&block)
